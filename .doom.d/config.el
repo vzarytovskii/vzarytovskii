1ff5bf37985553db3ce5f75ab3396803e517d664
Vlad Zarytovskii <vzaritovsky@hotmail.com>
2020-02-01 22:27:42 +0100

    Cleaned up config, added missing packages

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -1,53 +1,13 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+(setq user-full-name "Vlad Zarytovskii"
+      user-mail-address "vzaritovsky@hotmail.com")
 
-;; Place your private configuration here! Remember, you do not need to run 'doom
-;; refresh' after modifying this file!
-
-
-;; These are used for a number of things, particularly for GPG configuration,
-;; some email clients, file templates and snippets.
-(setq user-full-name "John Doe"
-      user-mail-address "john@doe.com")
-
-;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
-;; are the three important ones:
-;;
-;; + `doom-font'
-;; + `doom-variable-pitch-font'
-;; + `doom-big-font' -- used for `doom-big-font-mode'
-;;
-;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
-;; font string. You generally only need these two:
 (setq doom-font (font-spec :family "monospace" :size 14))
-
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. These are the defaults.
 (setq doom-theme 'doom-one)
-
-;; If you intend to use org, it is recommended you change this!
 (setq org-directory "~/org/")
 
-;; If you want to change the style of line numbers, change this to `relative' or
-;; `nil' to disable it:
-(setq display-line-numbers-type t)
+(setq display-line-numbers-type 'relative)
 (setq show-trailing-whitespace t)
-
-;; Here are some additional functions/macros that could help you configure Doom:
-;;
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package' for configuring packages
-;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', where Emacs
-;;   looks when you load packages with `require' or `use-package'.
-;; - `map!' for binding new keys
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c g k').
-;; This will open documentation for it, including demos of how they are used.
-;;
-;; You can also try 'gd' (or 'C-c g d') to jump to their definition and see how
-;; they are implemented.
 
 (use-package! powershell
   :after org
@@ -73,6 +33,7 @@
     (global-set-key (kbd "C-S-p") 'snails3))
 
 (use-package! lsp-ui
+  :after lsp-mode
   :hook (lsp-mode . lsp-ui-mode)
   :config
     (set-lookup-handlers! 'lsp-ui-mode
@@ -85,6 +46,18 @@
           lsp-ui-sideline-show-hover t)
     (global-set-key (kbd "F12") #'lsp-ui-peek-find-definitions)
     (global-set-key (kbd "C-F12") #'lsp-ui-peek-find-references))
+
+(def-package! company-lsp
+  :after lsp-mode
+  :config
+  (set-company-backend! 'lsp-mode 'company-lsp)
+  (setq company-lsp-enable-recompletion t))
+
+(use-package! dap-mode
+  :after lsp-mode
+  :config
+  (dap-mode t)
+  (dap-ui-mode t))
 
 (use-package! focus
   :config '((prog-mode . defun) (text-mode . sentence)))
