bbebef149fbfa373d1b13304c5733ea45e4fa6f2
Vlad Zarytovskii <vzaritovsky@hotmail.com>
2020-05-28 11:08:17 +0200

    Emacs: remap multiple cursors to <C-d>, since I'm not using the original binding

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -283,6 +283,7 @@ region-end is used."
   :init
   (setq mc/always-run-for-all t)
   (global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+  (global-set-key (kbd "C-d") 'mc/mark-next-like-this)
   (global-set-key (kbd "C->") 'mc/mark-next-like-this)
   (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
   (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this))
