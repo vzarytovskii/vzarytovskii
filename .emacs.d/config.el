f8424a486c19707fc3844470f2bd8b190da99686
Vlad Zarytovskii <vzaritovsky@hotmail.com>
2020-08-26 11:25:28 +0200

    Updated dotfiles

--- .emacs.d/config.el
+++ .emacs.d/config.el
@@ -204,8 +204,7 @@ region-end is used."
 
 (use-package dashboard
   :config
-  (setq initial-buffer-choice (lambda () (get-buffer "*dashboard*"))
-        dashboard-items '((recents  . 5)
+  (setq dashboard-items '((recents  . 5)
                           (bookmarks . 5)
                           (projects . 5)
                           (agenda . 5)
