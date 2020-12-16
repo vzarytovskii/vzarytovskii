fa88c43f54b9d2f3c6dba9db7e9a1e8a546ca576
Vlad Zarytovskii <vzaritovsky@hotmail.com>
2020-12-16 14:28:03 +0100

    Emacs: added some parens and indent related settings

--- .emacs.d/ui.el
+++ .emacs.d/ui.el
@@ -156,6 +156,12 @@
   :delight
   :hook (prog-mode-hook . unicode-troll-stopper-mode))
 
+(use-package aggressive-indent
+  :hook (emacs-lisp-mode-hook . aggressive-indent-mode)
+  :hook (csharp-mode-hook . aggressive-indent-mode)
+  :hook (fsharp-mode-hook . aggressive-indent-mode)
+  :hook (css-mode-hook . aggressive-indent-mode))
+
 (use-package paren
   :config
   (setq show-paren-style 'parenthesis
@@ -206,8 +212,126 @@ FACE defaults to inheriting from default and highlight."
   (advice-add #'show-paren-function :after #'show-paren-off-screen)
   :hook (after-init-hook . show-paren-mode))
 
+(use-package smartparens
+  :delight
+  :config
+  (progn
+    (require 'smartparens-config)
+    (smartparens-global-mode 1)
+    (show-paren-mode t)))
+
+(use-package parinfer
+  :delight
+  :init
+  (progn
+    (setq parinfer-extensions
+          '(defaults       ; should be included.
+             pretty-parens  ; different paren styles for different modes.
+             lispy          ; If you use Lispy. With this extension, you should install Lispy and do not enable lispy-mode directly.
+             paredit        ; Introduce some paredit commands.
+             smart-tab      ; C-b & C-f jump positions and smart shift with tab & S-tab.
+             smart-yank))   ; Yank behavior depend on mode.
+    (add-hook 'clojure-mode-hook #'parinfer-mode)
+    (add-hook 'emacs-lisp-mode-hook #'parinfer-mode)
+    (add-hook 'common-lisp-mode-hook #'parinfer-mode)
+    (add-hook 'lisp-mode-hook #'parinfer-mode)))
+
 (use-package rainbow-delimiters
   :hook (prog-mode-hook . rainbow-delimiters-mode))
+
+(use-package awesome-pair
+  :straight (:host github :repo "manateelazycat/awesome-pair" :branch "master")
+  :bind ((:map awesome-pair-mode-map
+               ("(" . 'awesome-pair-open-round)
+               ("[" . 'awesome-pair-open-bracket)
+               ("{" . 'awesome-pair-open-curly)
+               (")" . 'awesome-pair-close-round)
+               ("]" . 'awesome-pair-close-bracket)
+               ("}" . 'awesome-pair-close-curly)
+               ("%" . 'awesome-pair-match-paren)
+               ("\"" . 'awesome-pair-double-quote)
+               ("DEL" . 'awesome-pair-backward-delete)
+               ("C-k" . 'awesome-pair-kill)
+               ("M-\"" . 'awesome-pair-wrap-double-quote)
+               ("M-[" . 'awesome-pair-wrap-bracket)
+               ("M-{" . 'awesome-pair-wrap-curly)
+               ("M-(" . 'awesome-pair-wrap-round)
+               ("M-]" . 'awesome-pair-unwrap)
+               ("M-n" . 'awesome-pair-jump-right)
+               ("M-p" . 'awesome-pair-jump-left)
+               ("M-RET" . 'awesome-pair-jump-out-pair-and-newline)))
+  :hook (((prog-mode web-mode conf-mode yaml-mode editorconfig-mode vue-mode) . awesome-pair-mode)
+         ((c++-mode java-mode rust-mode) . (lambda () (local-set-key (kbd "<") '+prog/insert-angle)))
+         (rust-mode . (lambda () (local-set-key (kbd "|") '+prog/insert-rust-closure))))
+  :config
+  (defun awesome-pair-in-string-p-advice (&optional state)
+    (unless (or (bobp) (eobp))
+      (save-excursion
+        (or
+         (and
+          (nth 3 (or state (awesome-pair-current-parse-state)))
+          (not (equal (point) (line-end-position))))
+         (and
+          (eq (get-text-property (point) 'face) 'font-lock-string-face)
+          (eq (get-text-property (- (point) 1) 'face) 'font-lock-string-face))
+         (and
+          (eq (get-text-property (point) 'face) 'font-lock-doc-face)
+          (eq (get-text-property (- (point) 1) 'face) 'font-lock-doc-face))
+         ;; fix single quote pair delete for c/c++/java-mode
+         (and
+          (eq ?\" (char-syntax (char-before)))
+          (eq ?\" (char-syntax (char-after (point)))))))))
+
+  (advice-add 'awesome-pair-in-string-p :override 'awesome-pair-in-string-p-advice)
+
+  (defun +prog/insert-angle ()
+    "Insert angle brackets like intellij idea."
+    (interactive)
+    (save-excursion
+      (let ((pos (point))
+            (bounds (bounds-of-thing-at-point 'symbol)))
+        (if bounds
+            (let ((letter (char-after (car bounds))))
+              (if (and (eq (upcase letter) letter)
+                       (not (eq (downcase letter) letter)))
+                  (insert "<>")
+                (insert "<")))
+          (insert "<"))))
+    (forward-char))
+
+  (defun +prog/insert-rust-closure ()
+    (interactive)
+    (save-excursion
+      (if (and (equal major-mode 'rust-mode)
+               (eq ?\( (char-before)))
+          (insert "||")
+        (insert "|")))
+    (forward-char))
+
+  (defun +prog/in-empty-pair-p (awesome-in-empty-pair-fn &rest args)
+    (or (funcall awesome-in-empty-pair-fn)
+        (and (eq ?> (char-after))
+             (eq ?< (char-before)))
+        (and (equal major-mode 'rust-mode)
+             (eq ?| (char-after))
+             (eq ?| (char-before)))))
+
+  (advice-add 'awesome-pair-in-empty-pair-p :around '+prog/in-empty-pair-p)
+
+  (defun +prog/fix-unbalanced-parentheses-or-forward-char ()
+    "Fix missing close pair or just move forward one character."
+    (interactive)
+    (let ((close (awesome-pair-missing-close)))
+      (if close
+          (cond ((eq ?\) (matching-paren close))
+                 (insert ")"))
+                ((eq ?\} (matching-paren close))
+                 (insert "}"))
+                ((eq ?\] (matching-paren close))
+                 (insert "]")))
+        (forward-char))))
+
+  (advice-add 'awesome-pair-fix-unbalanced-parentheses :override '+prog/fix-unbalanced-parentheses-or-forward-char))
 
 (provide 'ui)
 ;;; ui.el ends here
