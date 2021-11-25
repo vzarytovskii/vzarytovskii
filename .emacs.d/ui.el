5ed99fb131668a07522cb6c39c2705fecf8134bb
Vlad Zarytovskii <vzaritovsky@hotmail.com>
2021-11-25 14:02:51 +0100

    Emacs: cleanup dimmer config

--- .emacs.d/ui.el
+++ .emacs.d/ui.el
@@ -162,27 +162,19 @@
 
 (use-package dimmer
   :config
-  (setq dimmer-exclusion-predicates '(window-minibuffer-p)
+  (setq dimmer-adjustment-mode :both
+	dimmer-watch-frame-focus-events t
+        dimmer-exclusion-predicates '(window-minibuffer-p)
         dimmer-exclusion-regexp-list '("^\\*Minibuf-[0-9]+\\*" "^*Messages*")
         dimmer-fraction 0.35)
-  (defun dimmer-off ()
-    (dimmer-process-all)
-    (dimmer-mode -1))
 
-  (defun dimmer-on ()
-    (dimmer-mode 1)
-    (dimmer-process-all))
-
-  (add-hook 'focus-out-hook #'dimmer-off)
-  (add-hook 'focus-in-hook  #'dimmer-on)
-  (add-hook 'minibuffer-setup-hook #'dimmer-off)
-  (add-hook 'minibuffer-exit-hook  #'dimmer-on)
-
-  (dimmer-mode t))
-
+  (dimmer-configure-company-box)
   (dimmer-configure-which-key)
   (dimmer-configure-org)
-  (dimmer-configure-posframe))
+  (dimmer-configure-posframe)
+  (dimmer-configure-magit)
+
+  (dimmer-mode t))
 
 (use-package solaire-mode
   :hook (after-init-hook . solaire-global-mode)
