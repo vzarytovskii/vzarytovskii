422a75119fe5439241cbef2038b1068e9c1805cc
Vlad Zarytovskii <vzaritovsky@hotmail.com>
2023-02-06 15:45:14 +0100

    NVIM: added git completions

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -100,6 +100,7 @@ packer.startup({function(use)
       'hrsh7th/cmp-nvim-lsp-document-symbol',
       'hrsh7th/cmp-nvim-lsp-signature-help',
       'hrsh7th/cmp-calc',
+      'petertriho/cmp-git',
       'saadparwaiz1/cmp_luasnip',
   }
 
@@ -1512,6 +1513,7 @@ nvim_cmp.setup {
     { name = 'nvim_lsp_signature_help' },
     { name = 'nvim_lsp' },
     { name = 'luasnip' },
+    { name = 'git' }
   },{
     { name = 'buffer' },
     { name = 'calc' },
@@ -1596,6 +1598,69 @@ end)
 nvim_cmp.event:on("menu_closed", function ()
   vim.b.copilot_suggestion_hidden = false
 end)
+
+local format = require("cmp_git.format")
+local sort = require("cmp_git.sort")
+
+require("cmp_git").setup({
+    -- defaults
+    filetypes = { "gitcommit", "octo" },
+    remotes = { "upstream", "origin" }, -- in order of most to least prioritized
+    enableRemoteUrlRewrites = false, -- enable git url rewrites, see https://git-scm.com/docs/git-config#Documentation/git-config.txt-urlltbasegtinsteadOf
+    git = {
+        commits = {
+            limit = 100,
+            sort_by = sort.git.commits,
+            format = format.git.commits,
+        },
+    },
+    github = {
+        issues = {
+            fields = { "title", "number", "body", "updatedAt", "state" },
+            filter = "all", -- assigned, created, mentioned, subscribed, all, repos
+            limit = 100,
+            state = "open", -- open, closed, all
+            sort_by = sort.github.issues,
+            format = format.github.issues,
+        },
+        mentions = {
+            limit = 100,
+            sort_by = sort.github.mentions,
+            format = format.github.mentions,
+        },
+        pull_requests = {
+            fields = { "title", "number", "body", "updatedAt", "state" },
+            limit = 100,
+            state = "open", -- open, closed, merged, all
+            sort_by = sort.github.pull_requests,
+            format = format.github.pull_requests,
+        },
+    },
+    trigger_actions = {
+        {
+            debug_name = "git_commits",
+            trigger_character = ":",
+            action = function(sources, trigger_char, callback, params, git_info)
+                return sources.git:get_commits(callback, params, trigger_char)
+            end,
+        },
+        {
+            debug_name = "github_issues_and_pr",
+            trigger_character = "#",
+            action = function(sources, trigger_char, callback, params, git_info)
+                return sources.github:get_issues_and_prs(callback, git_info, trigger_char)
+            end,
+        },
+        {
+            debug_name = "github_mentions",
+            trigger_character = "@",
+            action = function(sources, trigger_char, callback, params, git_info)
+                return sources.github:get_mentions(callback, git_info, trigger_char)
+            end,
+        },
+    },
+  }
+)
 
 mason.setup({
     PATH = "prepend",
