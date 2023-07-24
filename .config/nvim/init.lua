a9f6dae8ee4fe48b1875c8649720380cda3a2875
Vlad Zarytovskii <vzaritovsky@hotmail.com>
2023-07-24 16:38:51 +0200

    NVIM: Change theme and layout a bit

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -127,24 +127,53 @@ vim.api.nvim_create_autocmd({ "CursorHold" }, {
 
 require("lazy").setup({
   {
-    "folke/tokyonight.nvim",
+    'projekt0n/github-nvim-theme',
     lazy = false,
     priority = 1000,
     config = function()
-      local tokyonight = require("tokyonight")
-      tokyonight.setup({
-        style = "day",
-        light_style = "day",
-        styles = {
-          comments = { italic = false },
-          keywords = { italic = false },
-          functions = {},
-          variables = {},
-          sidebars = "dark",
-          floats = "dark",
-        }
+      require('github-theme').setup({
+        options = {
+          compile_path = vim.fn.stdpath('cache') .. '/github-theme',
+          compile_file_suffix = '_compiled', -- Compiled file suffix
+          hide_end_of_buffer = true, -- Hide the '~' character at the end of the buffer for a cleaner look
+          hide_nc_statusline = true, -- Override the underline style for non-active statuslines
+          transparent = false,       -- Disable setting background
+          terminal_colors = true,    -- Set terminal colors (vim.g.terminal_color_*) used in `:terminal`
+          dim_inactive = false,      -- Non focused panes set to alternative background
+          module_default = true,     -- Default enable value for modules
+          styles = {                 -- Style to be applied to different syntax groups
+            comments = 'NONE',       -- Value is any valid attr-list value `:help attr-list`
+            functions = 'NONE',
+            keywords = 'NONE',
+            variables = 'NONE',
+            conditionals = 'NONE',
+            constants = 'NONE',
+            numbers = 'NONE',
+            operators = 'NONE',
+            strings = 'NONE',
+            types = 'NONE',
+          },
+          inverse = {                -- Inverse highlight for different types
+            match_paren = false,
+            visual = false,
+            search = false,
+          },
+          darken = {                 -- Darken floating windows and sidebar-like windows
+            floats = false,
+            sidebars = {
+              enable = true,
+              list = {},             -- Apply dark background to specific windows
+            },
+          },
+          modules = {                -- List of various plugins and additional options
+            -- ...
+          },
+        },
+        palettes = {},
+        specs = {},
+        groups = {},
       })
-      vim.cmd([[colorscheme tokyonight]])
+      vim.cmd([[colorscheme github_light_high_contrast]])
     end,
   },
   {
@@ -227,8 +256,8 @@ require("lazy").setup({
       gitsigns.setup {
         signcolumn = true,
         numhl      = true,
-        linehl     = true,
-        word_diff  = true,
+        linehl     = false,
+        word_diff  = false,
         current_line_blame = false,
         current_line_blame_opts = {
           virt_text = true,
@@ -657,6 +686,7 @@ require("lazy").setup({
   },
   {
     'stevearc/aerial.nvim',
+    -- cond = false, -- Disable, it's not that useful as for now.
     config = function ()
       local aerial = require('aerial')
       aerial.setup({
@@ -687,13 +717,40 @@ require("lazy").setup({
           max_height = 0.9,
           relative = "editor",
           override = function(conf, source_winid)
-            local padding = 0
+            local padding = 1
             conf.anchor = 'NE'
             conf.row = padding
             conf.col = vim.o.columns - padding
             return conf
           end,
         }
+      })
+    end
+  },
+  {
+    'dgagn/diagflow.nvim',
+    cond = false,
+    opts = {},
+    config = function ()
+      require('diagflow').setup({
+          enable = true,
+          max_width = 60,  -- The maximum width of the diagnostic messages
+          severity_colors = {  -- The highlight groups to use for each diagnostic severity level
+              error = "DiagnosticFloatingError",
+              warning = "DiagnosticFloatingWarn",
+              info = "DiagnosticFloatingInfo",
+              hint = "DiagnosticFloatingHint",
+          },
+          gap_size = 1,
+          scope = 'cursor', -- 'cursor', 'line'
+          padding_top = 0,
+          padding_right = 0,
+          text_align = 'right', -- 'left', 'right'
+          placement = 'top', -- 'top', 'inline'
+          inline_padding_left = 0, -- the padding left when the placement is inline
+          update_event = { 'DiagnosticChanged' }, -- the event that updates the diagnostics cache
+          toggle_event = { }, -- if InsertEnter, can toggle the diagnostics on inserts
+          show_sign = false, -- set to true if you want to render the diagnostic sign before the diagnostic message
       })
     end
   },
