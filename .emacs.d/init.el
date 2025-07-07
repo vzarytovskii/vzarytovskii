a51008affde40185f4e37cf178fbcd46ff4eb037
Vlad Zarytovskii <vzaritovsky@hotmail.com>
2025-07-07 18:11:00 +0200

    Emacs: Cleanup config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -113,11 +113,13 @@
         browse-url-generic-args     '("/c" "start")
         browse-url-browser-function #'browse-url-generic))
 
-(use-package esup :defer t)
+(use-package esup)
 
-(use-package llama :defer t)
-(use-package closql :defer t)
-(use-package emacsql :defer t)
+(use-package llama)
+(use-package closql)
+(use-package emacsql)
+
+(use-package hydra)
 
 (use-package exec-path-from-shell
   :when (eq system-type 'darwin)
@@ -511,7 +513,21 @@
     :init
     (global-lsp-bridge-mode))
 
-  ;; Git
+  (use-package whitespace-mode :ensure nil)
+
+  ;; VC
+  (use-package vc-mode
+    :ensure nil)
+
+  (use-package diff-mode
+    :ensure nil
+    :hook (diff-mode-hook .(lambda ()
+                            (setq-local whitespace-style
+                                        '( face tabs tab-mark spaces space-mark trailing
+                                          indentation::space indentation::tab
+                                          newline newline-mark))
+                            (whitespace-mode)))
+    :after whitespace-mode)
 
   (use-package diff-hl
     :config
@@ -539,6 +555,46 @@
       (with-eval-after-load 'desktop
         (add-to-list 'desktop-minor-mode-table
                     '(diff-hl-margin-mode nil))))))
+
+  (use-package smerge-mode
+    :ensure nil
+    :config
+    (defhydra smerge-hydra
+      (:color pink :hint nil :post (smerge-auto-leave))
+      "
+  ^Move^       ^Keep^               ^Diff^                 ^Other^
+  ^^-----------^^-------------------^^---------------------^^-------
+  _n_ext       _b_ase               _<_: upper/base        _C_ombine
+  _p_rev       _u_pper              _=_: upper/lower       _r_esolve
+  ^^           _l_ower              _>_: base/lower        _k_ill current
+  ^^           _a_ll                _R_efine
+  ^^           _RET_: current       _E_diff
+  "
+      ("n" smerge-next)
+      ("p" smerge-prev)
+      ("b" smerge-keep-base)
+      ("u" smerge-keep-upper)
+      ("l" smerge-keep-lower)
+      ("a" smerge-keep-all)
+      ("RET" smerge-keep-current)
+      ("\C-m" smerge-keep-current)
+      ("<" smerge-diff-base-upper)
+      ("=" smerge-diff-upper-lower)
+      (">" smerge-diff-base-lower)
+      ("R" smerge-refine)
+      ("E" smerge-ediff)
+      ("C" smerge-combine-with-next)
+      ("r" smerge-resolve)
+      ("k" smerge-kill-current)
+      ("ZZ" (lambda ()
+              (interactive)
+              (save-buffer)
+              (bury-buffer))
+      "Save and bury buffer" :color blue)
+      ("q" nil "cancel" :color blue))
+    :hook (magit-diff-visit-file . (lambda ()
+                                    (when smerge-mode
+                                      (smerge-hydra/body)))))
 
   (use-package magit
     :commands magit-status
